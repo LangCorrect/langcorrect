aa11f9b87151fc53420b86e38b93ef644fb32e9d
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2025-01-26 00:35:57 +0900

    chore: update site metadata

---
+++ config/site.ts
@@ -0,0 +1,31 @@
+export const siteConfig = {
+    name: "LangCorrect",
+    authors: [
+        { name: "LangCorrect Team", url: "https://langcorrect.com" },
+        { name: "Daniel Zeljko", url: "https://github.com/danielzeljko" },
+    ],
+    url: "https://langcorrect.com/",
+    keywords: [
+        "Language learning",
+        "Language exchange",
+        "Correct my writing",
+        "Writing correction platform",
+        "Language practice",
+        "Improve writing skills",
+        "Language learning community",
+        "Grammar corrections",
+        "Writing feedback",
+        "Peer language corrections",
+        "Native speaker corrections",
+        "Language exchange platform",
+    ],
+    description:
+        "Master grammar, spelling, and syntax in the language(s) you're learning through direct feedback on your writing from fluent, native speakers.",
+    links: {
+        twitter: "https://twitter.com/langcorrect",
+        instagram: "https://www.instagram.com/langcorrect/",
+        github: "https://github.com/LangCorrect",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
