82da924c178c55951a50207a084c7886de7ce0ea
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2025-01-26 00:58:28 +0900

    style: change font to open sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Open_Sans } from "next/font/google";
 import { siteConfig } from "@/config/site";
+import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const openSans = Open_Sans({
     subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+    variable: "--font-open-sans",
 });
 
 export const metadata: Metadata = {
@@ -29,9 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
+            <body className={`${openSans.variable} antialiased`}>
                 {children}
             </body>
         </html>
