5341f75e6443d49dfb1b92fbcff10b701152dc19
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2025-02-04 15:40:35 +0900

    feat: change site theme to dracula

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" data-theme="dark">
+        <html lang="en" data-theme="dracula">
             <body className={`${openSans.className} antialiased`}>
                 <Navbar />
                 {children}
